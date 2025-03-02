d8546ba23c01ef1c629ded89dfb88f6d7c3165a0
Jose117053 <adolfo10420@gmail.com>
2025-03-01 20:47:32 -0600

    Delete

--- src/pastel.py
+++
@@ -1,7 +0,0 @@
-class Pastel:
-    def __init__(self, nombre, color):
-        self.nombre = nombre
-        self.color = color
-
-    def __str__(self):
-        return f"{self.color}{self.nombre}\033[0m"  # Aplica el color y lo resetea al final
